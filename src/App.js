a139ce51f364a5af1621e8b557f35ffc2fecfb13
Felipe Borges <fwbs27@gmail.com>
2020-05-30 18:54:26 -0300

    Adds new text change in index page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Opa! Olá!        
+         Opa! Olá! Este é um projeto para estudo sobre docker em um ambiente da aws com kubernetes.      
         </p>
         <a
           className="App-link"
